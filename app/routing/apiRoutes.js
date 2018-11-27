2baee910527a639199278b06cb8df1ad95cb4a83
matt fyke <mfyke@live.com>
2018-11-26 23:35:03 -0800

    friend match working

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -3,5 +3,27 @@ module.exports = (app)=> {
 	app.get("/api/friends", (req,res) => {
 		res.json(friends);
 	});
+	app.post("/api/friends", (req,res) => {
+		const currentProf = req.body;
+		const differences = [];
 
+		console.log(currentProf);
+
+		for(let i = 0; i < friends.length ; i++) {
+			let total = 0;
+			for(let j=0; j < friends[i].scores.length; j++) {
+				total+=Math.abs(parseInt(friends[i].scores[j])-parseInt(req.body.scores[j]));
+			}
+			differences.push(total);
+		}
+		let index = 0;
+		let bestFriend = differences[0];
+		for(let k = 0; k<differences.length; k++) {
+			if(differences[k]<bestFriend) {
+				bestFriend=differences[k];
+				index=k;
+			}
+		}
+		res.json(friends[index]);
+	});
 }
--- server.js
+++ server.js
@@ -5,8 +5,15 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" })); 
+
 require("./app/routing/apiRoutes.js") (app);
 require("./app/routing/htmlRoutes.js") (app);
+
+
 
 app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
 
